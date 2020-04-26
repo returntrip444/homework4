1e0d7eb57ed473dcf77b24c00d0019d0cdb07b79
returntrip444 <danp777991@gmail.com>
2020-04-26 09:11:59 -0700

    tried adding timer to code

---
+++ homework4.js
@@ -0,0 +1,25 @@
+
+
+// Creating array for correct Answers, wrong Answers, and questions
+
+var codeCorrectAnswers =  ["javascript", "cascading style sheet", "img href", "set-animate", "Console log" ];
+var codeQuestions = ["what is the most common computer language for modifying a web page", "what does CSS stand for?", "How do you refrence an image in a HTML?", "How do you make an image move in an html?", "where in the inspect page can you find strings and arrays?"];
+var codeWrongAnswers = ["DOM", "CSS", "Jquery", "HTML", "Consumer style sheet", "cascading syntax sheet", "console syntax sheet", "computer style sheet", "img-src", "img.setAttribute", "console.log", "img.QuerySelector", "set-ChildDiv", "img-href", "img-src","document.getElementById", "Application", "Inspect", "Dev tools", "navigator"];
+var timeEl = ["6000 miliseconds"]
+
+// set time for code quiz to 60 secondsLeft
+
+
+function setTime () {
+    secondsLeft= (i < timeEl.length i++)
+    timeEl.textContent
+    var imgEl= document.createElement("img")
+    window.setTimeout( setTime, "6000 milliseconds");
+
+}
+
+// put in window.setTimeout for time ended after question
+
+
+
+
