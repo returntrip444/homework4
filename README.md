bab08ac67d75014546eda9e1dc7fcd9155332166
returntrip444 <danp777991@gmail.com>
2020-04-27 09:02:05 -0700

    created loops for questions and true and false answers

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 beginning stages of code quiz
 
 creating timer, answers, questions, and new pages to direct whether answers are correct or wrong
+
+created array for codeTime, codeCorrectAnswers, codeWrongAnswers, codeQuestions
+
+created question answers and true or falso statements use using var questions followed by questions and correct answers and also a loop
+for questions length using  for (var i = 0; i < codeQuestions.length; i++
--- homework4.js
+++ homework4.js
@@ -2,7 +2,7 @@
 
 // Creating array for correct Answers, wrong Answers, and questions, time
 
-var codeCorrectAnswers =  ["javascript", "cascading style sheet", "img href", "set-animate", "Console log" ];
+var codeCorrectAnswers =  ["javascript", "cascading style sheet", "img href", "set-animate", "Console" ];
 var codeQuestions = ["what is the most common computer language for modifying a web page", "what does CSS stand for?", "How do you refrence an image in a HTML?", "How do you make an image move in an html?", "where in the inspect page can you find strings and arrays?"];
 var codeWrongAnswers = ["DOM", "CSS", "Jquery", "HTML", "Consumer style sheet", "cascading syntax sheet", "console syntax sheet", "computer style sheet", "img-src", "img.setAttribute", "console.log", "img.QuerySelector", "set-ChildDiv", "img-href", "img-src","document.getElementById", "Application", "Inspect", "Dev tools", "navigator"];
 var timeEl = ["6000 miliseconds"]
@@ -32,6 +32,37 @@ function  () {
                 newItem.textContent = codeQuestions[i]
                 codeQuestions.appendChild(newItem)
 
+
+        // The array of questions for our quiz game.
+    var questions = [
+        { q: "what is the most common computer language for modifying a webpage", a: "javascript" },
+        { q: "what does css stand for?", a: "cascading style sheet" },
+        { q: "how do you refrence an image in html?", a: "img-href" },
+        { q: "how do you make an image move in html?", a: "set-animate" },
+        { q: "where in the inspect page can you find strings and arrays?", a: "console" }
+      ];
+  
+      // We start the game with a score of 0.
+      var score = 0;
+  
+      // Loop over every question object
+      for (var i = 0; i < codeQuestions.length; i++) {
+        var answer = confirm(questions[i].q);
+  
+    //    Comapring right and wrong answers with === true or false statements
+        if ((answer === true && questions[i].a === "t") ||
+          (answer === false && questions[i].a === "f")) {
+         
+          score++;
+          alert("Correct!");
+        }
+        else {
+          alert("Wrong!");
+        }
+      }
+  
+    //   Ending score of quiz
+      alert("You got " + score + "/" + codeQuestions.length);
     }
 }
 renderToDo();
